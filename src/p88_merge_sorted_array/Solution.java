6774fd0d350d128cdf1a423482b0679f71138ad4
Maksim Rudiak <maxim.rudiak@gmail.com>
2023-06-23 18:03:31 +0300

    fix: #88 add increment

--- src/p88_merge_sorted_array/Solution.java
+++ src/p88_merge_sorted_array/Solution.java
@@ -1,5 +1,7 @@
 package p88_merge_sorted_array;
 
+import java.util.Arrays;
+
 public class Solution {
 
     public static void merge (int[] num1, int m, int[] num2, int n ) {
@@ -12,8 +14,14 @@ public class Solution {
                 num1[k--] = num1[i--];
             }
             else {
-                num1[k] = num2[j--];
+                num1[k--] = num2[j--];
             }
         }
+    }
+
+    public static void main(String[] args) {
+        int[] res = new int[]{1,2,3,0,0,0};
+        merge(res, 3, new int[]{2,5,5}, 3);
+        Arrays.stream(res).forEach(System.out::print);
     }
 }
