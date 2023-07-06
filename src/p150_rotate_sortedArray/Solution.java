653eeb28b00b087c43ff5fb5f5828227994ebc97
Maksim Rudiak <maxim.rudiak@gmail.com>
2023-07-06 21:21:17 +0300

    feat: add #150 rotate sorted array

---
+++ src/p150_rotate_sortedArray/Solution.java
@@ -0,0 +1,35 @@
+package p150_rotate_sortedArray;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public static void rotate(int[] nums, int k) {
+        if (nums == null || nums.length < 2) {
+            return;
+        }
+        k %= nums.length;
+        if (k != 0) {
+            reverse(nums, 0, nums.length - 1);
+            reverse(nums, 0, k - 1);
+            reverse(nums, k, nums.length - 1);
+        } else {
+            return;
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        rotate(new int[]{1,2,3,4,5,6,7}, 3);
+    }
+}
