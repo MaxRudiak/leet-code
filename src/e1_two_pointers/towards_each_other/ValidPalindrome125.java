71ba6f7d1232a4e13bad0000b4c5b5345db827ce
Maksim Rudiak <maxim.rudiak@gmail.com>
2025-10-03 02:47:57 +0300

    feat: add #125 validPalindrome

---
+++ src/e1_two_pointers/towards_each_other/ValidPalindrome125.java
@@ -0,0 +1,33 @@
+package e1_two_pointers.towards_each_other;
+
+//A phrase is a palindrome if, after converting all uppercase letters
+// into lowercase letters and removing all non-alphanumeric characters,
+// it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+public class ValidPalindrome125 {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
+    }
+
+    public static boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
